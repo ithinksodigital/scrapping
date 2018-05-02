90b2c35c4bc773f4d8a93bde2b5e670d9f5bae6e
radipawelec <rafal@radipawelec.pl>
2018-05-02 22:42:45 +0200

    pa

--- antalpl.py
+++ antalpl.py
@@ -79,6 +79,8 @@ def antal_scrap():
 
 
 def antal_export(company_name):
+    path = '/home/jobad/scrapping/export/{}.csv'.format( company_name )
+
     global a
     test = []
 
@@ -90,14 +92,14 @@ def antal_export(company_name):
         test.append( event_obj )
 
     try:
-        os.remove( 'export/%s.csv' %company_name )
-        with open( 'export/%s.csv' %company_name, 'w', newline='', encoding='utf-8') as csvfile:
+        os.remove( path )
+        with open( path, 'w', newline='', encoding='utf-8') as csvfile:
             fields = ['category', 'offerts']
             writer = csv.DictWriter( csvfile, fieldnames=fields, delimiter=';' )
             writer.writeheader()
             writer.writerows( test )
     except:
-        with open( 'export/%s.csv' %company_name, 'w', newline='', encoding='utf-8' ) as csvfile:
+        with open( path, 'w', newline='', encoding='utf-8' ) as csvfile:
             fields = ['company_name', 'category', 'offers']
             writer = csv.DictWriter( csvfile, fieldnames=fields, delimiter=';' )
             writer.writeheader()
